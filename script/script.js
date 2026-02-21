604f3a05947178285ecd9154a68661f021d69177
Minhaj-Hossain <minhaj1080@gmail.com>
2026-02-22 03:07:46 +0600

    added all job counter function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 // console.log('welcome to the assignment 4')
+
+let totalCards = document.getElementById('totalCards');
+let cardCounter = document.getElementById('card-counter');
+const allBtn = document.getElementById('all-btn');
+
+
+
+function totalCardCounter() {
+
+    totalCards.innerText = cardCounter.children.length;
+}
+
+allBtn.addEventListener('click', totalCardCounter())
+
